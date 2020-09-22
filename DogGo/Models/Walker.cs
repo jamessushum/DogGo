55b3021481982fdac7bac9382996c792020a10d4
James Su-Shum <sushumjames@gmail.com>
2020-09-22 14:43:29 -0400

    Added some validation annotation to Walker model properties

--- DogGo/Models/Walker.cs
+++ DogGo/Models/Walker.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +10,19 @@ namespace DogGo.Models
     public class Walker
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(35)]
         public string Name { get; set; }
+
+        [Required]
+        [DisplayName("Neighborhood")]
         public int NeighborhoodId { get; set; }
+
+        [Required]
+        [DisplayName("Image")]
         public string ImageUrl { get; set; }
+
         public Neighborhood Neighborhood { get; set; }
     }
 }
