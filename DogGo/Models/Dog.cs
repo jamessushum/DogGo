7de983eb12f7130eb8b2b0088f61760cf6d66f6b
James Su-Shum <sushumjames@gmail.com>
2020-09-22 14:29:05 -0400

    Added some validation annotations to Dog model properties

--- DogGo/Models/Dog.cs
+++ DogGo/Models/Dog.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +10,22 @@ namespace DogGo.Models
     public class Dog
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Dogs need names too!")]
+        [MaxLength(35)]
         public string Name { get; set; }
+
+        [Required]
         public string Breed { get; set; }
+
         public string Notes { get; set; }
+
+        [DisplayName("Image")]
         public string ImageUrl { get; set; }
+
+        [Required]
         public int OwnerId { get; set; }
+
         public Owner Owner { get; set; }
     }
 }
